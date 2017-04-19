6468f6a0a91b27254813740dbd8b76cef504d009
Nathan LaFreniere <quitlahok@gmail.com>
2017-04-18 17:17:56 -0700

    i dont even know :sleeping:

--- README.md
+++ README.md
@@ -1,22 +1,75 @@
-## subpar
+## Subpar
 
 This module wraps [hapi](https://github.com/hapijs/hapi) to simplify its usage as a receiver of google's cloud pubsub push subscriptions.
 
-### usage
+### `new Subpar(name, [options])`
+
+Create a new instance of `Subpar`. The `name` parameter is used as the service name in the logging plugin.
+
+`options` is an optional configuration object and may contain:
+
+- `path`: (string) the HTTP path the handler will listen on, defaults to `"/"`
+- `environment`: (string) used to configure the logger, when set to `"production"` logging will go through [good-google-cloud](https://github.com/nodesecurity/good-google-cloud), when set to `"test"` logging will be disabled, when set to any other value logging will go to the console. Defaults to the value of `process.env.NODE_ENV` or `"development"` if unset.
+- `connection`: (object) passed through to [hapi's server.connection() method](https://github.com/hapijs/hapi/blob/master/API.md#serverconnectionoptions)
+
+
+### `subpar.bind(context)`
+
+Passed through to [hapi's server.bind() method](https://github.com/hapijs/hapi/blob/master/API.md#serverbindcontext).
+
+
+### `subpar.decorate(type, property, method, [options])`
+
+Passed through to [hapi's server.decorate() method](https://github.com/hapijs/hapi/blob/master/API.md#serverdecoratetype-property-method-options).
+
+
+### `subpar.handle(handler, [condition])`
+
+`handler` is a standard [hapi handler](https://github.com/hapijs/hapi/blob/master/API.md#route-handler).
+
+`condition` is an optional object used to filter when a handler is used. It may contain the following properties:
+
+- `data`:  (object or joi validator) value or values contained in the message data that must match for this handler to be called
+- `attributes`: (object or joi validator) value or values contained in the message attributes that must match for this handler to be called
+
+Only *one* handler may be added without a condition since having no condition means that all valid payloads will be delivered to that handler. Multiple handlers may be added with conditions and the first matching handler will be called.
+
+
+### `server.initialize()`
+
+Finalize the routing table, register the logging plugins and initialize the underlying hapi server. Returns a promise.
+
+
+### `server.start()`
+
+Start the underlying hapi server, returns a promise.
+
+
+## Example
 
 ```js
 'use strict';
 
 const Subpar = require('subpar');
 
-const subscription = new Subpar('worker-name');
-subscription.handle({
-  handler: function (request, reply) {
+const server = new Subpar('example');
 
-    // do something with request.payload.message.attributes and request.payload.message.data
-    reply();
-  }
+server.handle(function (request, reply) {
+
+  reply({ handler: 'fallback' });
 });
 
-subscription.start();
+server.handle(function (request, reply) {
+
+  reply({ handler: 'create' });
+}, { attributes: { type: 'create' } });
+
+server.handle(function (request, reply) {
+
+  reply({ handler: 'update' });
+}, { attributes: { type: 'update' } });
+
+server.start();
 ```
+
+In the above server, messages published with a `"type"` attribute with a value of `"create"` will be delivered to the middle handler. Messages with a `"type"` of `"update"` will be delivered to the bottom handler. All other messages will be delivered to the top handler.
---
+++ test/index.js
@@ -0,0 +1,385 @@
+'use strict';
+
+const Subpar = require('../');
+
+const Code = require('code');
+const lab = exports.lab = require('lab').script();
+const expect = Code.expect;
+
+const describe = lab.describe;
+const it = lab.it;
+
+describe('subpar', () => {
+
+  it('throws when created with invalid options', (done) => {
+
+    expect(() => {
+
+      new Subpar('test', { random: 'nonsense' });
+    }).to.throw();
+    done();
+  });
+
+  it('defaults environment to NODE_ENV', (done) => {
+
+    const server = new Subpar('test');
+    const handler = function (request, reply) {
+
+      return reply('hello');
+    };
+    server.handle(handler);
+
+    expect(server.environment).to.equal('test');
+    done();
+  });
+});
+
+describe('bind', () => {
+
+  it('can bind values to handlers', () => {
+
+    const server = new Subpar('test');
+    server.bind({ test: { some: 'object' } });
+
+    server.handle(function (request, reply) {
+
+      reply(this.test);
+    });
+
+    return server.initialize().then(() => {
+
+      const payload = {
+        subscription: 'test',
+        message: {
+          messageId: '1234',
+          data: {},
+          attributes: {}
+        }
+      };
+
+      return server.server.inject({ method: 'post', url: '/', payload });
+    }).then((res) => {
+
+      expect(res.statusCode).to.equal(200);
+      expect(res.result).to.equal({ some: 'object' });
+    });
+  });
+});
+
+describe('decorate', () => {
+
+  it('can add decorators', () => {
+
+    const server = new Subpar('test');
+    server.decorate('request', 'test', () => ({ some: 'object' }));
+
+    const handler = function (request, reply) {
+
+      reply(request.test());
+    };
+
+    server.handle(handler);
+
+    return server.initialize().then(() => {
+
+      const payload = {
+        subscription: 'test',
+        message: {
+          messageId: '1234',
+          data: {},
+          attributes: {}
+        }
+      };
+
+      return server.server.inject({ method: 'post', url: '/', payload });
+    }).then((res) => {
+
+      expect(res.statusCode).to.equal(200);
+      expect(res.result).to.equal({ some: 'object' });
+    });
+  });
+});
+
+describe('handle', () => {
+
+  it('can add a default handler', () => {
+
+    const server = new Subpar('test');
+    const handler = function (request, reply) {
+
+      reply({ some: 'object' });
+    };
+
+    server.handle(handler);
+
+    return server.initialize().then(() => {
+
+      const payload = {
+        subscription: 'test',
+        message: {
+          messageId: '1234',
+          data: {},
+          attributes: {}
+        }
+      };
+
+      return server.server.inject({ method: 'post', url: '/', payload });
+    }).then((res) => {
+
+      expect(res.statusCode).to.equal(200);
+      expect(res.result).to.equal({ some: 'object' });
+    });
+  });
+
+  it('throws when trying to add a second default handler', (done) => {
+
+    const server = new Subpar('test');
+    const handler = function (request, reply) {
+
+      reply({ some: 'object' });
+    };
+
+    server.handle(handler);
+    expect(() => {
+
+      server.handle(handler);
+    }).to.throw();
+    done();
+  });
+
+  it('can add a handler with a condition', () => {
+
+    const server = new Subpar('test');
+    const handler = function (request, reply) {
+
+      return reply('hello');
+    };
+
+    server.handle(handler, { attributes: { type: 'test' } });
+
+    return server.initialize().then(() => {
+
+      const payload = {
+        subscription: 'test',
+        message: {
+          messageId: '1234',
+          data: {},
+          attributes: {
+            type: 'test'
+          }
+        }
+      };
+
+      return server.server.inject({ method: 'post', url: '/', payload });
+    }).then((res) => {
+
+      expect(res.statusCode).to.equal(200);
+      expect(res.result).to.equal('hello');
+    });
+  });
+
+  it('responds with 204 when condition does not match and no fallback has been added', () => {
+
+    const server = new Subpar('test');
+    const handler = function (request, reply) {
+
+      return reply('hello');
+    };
+
+    server.handle(handler, { attributes: { type: 'test' } });
+
+    return server.initialize().then(() => {
+
+      const payload = {
+        subscription: 'test',
+        message: {
+          messageId: '1234',
+          data: {},
+          attributes: {
+            type: 'unmatched'
+          }
+        }
+      };
+
+      return server.server.inject({ method: 'post', url: '/', payload });
+    }).then((res) => {
+
+      expect(res.statusCode).to.equal(204);
+    });
+  });
+
+  it('falls back to default handler when condition does not match', () => {
+
+    const server = new Subpar('test');
+    const handler = function (request, reply) {
+
+      return reply('hello');
+    };
+
+    const fallback = function (request, reply) {
+
+      return reply('fallback');
+    };
+
+    server.handle(handler, { attributes: { type: 'test' } });
+    server.handle(fallback);
+
+    return server.initialize().then(() => {
+
+      const payload = {
+        subscription: 'test',
+        message: {
+          messageId: '1234',
+          data: {},
+          attributes: {
+            type: 'test'
+          }
+        }
+      };
+
+      return server.server.inject({ method: 'post', url: '/', payload });
+    }).then((res) => {
+
+      expect(res.statusCode).to.equal(200);
+      expect(res.result).to.equal('hello');
+
+      const payload = {
+        subscription: 'test',
+        message: {
+          messageId: '1234',
+          data: {},
+          attributes: {
+            type: 'different'
+          }
+        }
+      };
+
+      return server.server.inject({ method: 'post', url: '/', payload });
+    }).then((res) => {
+
+      expect(res.statusCode).to.equal(200);
+      expect(res.result).to.equal('fallback');
+    });
+  });
+
+  it('decodes base64 encoded json', () => {
+
+    const server = new Subpar('test');
+    const handler = function (request, reply) {
+
+      expect(request.payload.data).to.equal({ encoded: 'json' });
+      reply({ some: 'object' });
+    };
+
+    server.handle(handler);
+
+    return server.initialize().then(() => {
+
+      const payload = {
+        subscription: 'test',
+        message: {
+          messageId: '1234',
+          data: Buffer.from(JSON.stringify({ encoded: 'json' })).toString('base64'),
+          attributes: {}
+        }
+      };
+
+      return server.server.inject({ method: 'post', url: '/', payload });
+    }).then((res) => {
+
+      expect(res.statusCode).to.equal(200);
+      expect(res.result).to.equal({ some: 'object' });
+    });
+  });
+
+  it('refuses to parse invalid base64 encoded json', () => {
+
+    const server = new Subpar('test');
+    const handler = function (request, reply) {
+
+      expect(request.payload.data).to.not.exist();
+      reply({ some: 'object' });
+    };
+
+    server.handle(handler);
+
+    return server.initialize().then(() => {
+
+      const payload = {
+        subscription: 'test',
+        message: {
+          messageId: '1234',
+          data: Buffer.from('{"encoded":"json"').toString('base64'),
+          attributes: {}
+        }
+      };
+
+      return server.server.inject({ method: 'post', url: '/', payload });
+    }).then((res) => {
+
+      expect(res.statusCode).to.equal(200);
+      expect(res.result).to.equal({ some: 'object' });
+    });
+  });
+
+  it('refuses to coerce numbers as base64 encoded json', () => {
+
+    const server = new Subpar('test');
+    const handler = function (request, reply) {
+
+      expect(request.payload.data).to.not.exist();
+      reply({ some: 'object' });
+    };
+
+    server.handle(handler);
+
+    return server.initialize().then(() => {
+
+      const payload = {
+        subscription: 'test',
+        message: {
+          messageId: '1234',
+          data: 1234,
+          attributes: {}
+        }
+      };
+
+      return server.server.inject({ method: 'post', url: '/', payload });
+    }).then((res) => {
+
+      expect(res.statusCode).to.equal(200);
+      expect(res.result).to.equal({ some: 'object' });
+    });
+  });
+
+  it('fails to initialize when no handlers have been added', () => {
+
+    const server = new Subpar('test');
+
+    return server.initialize().then(() => {
+
+      Code.fail('should not be reached');
+    }).catch((err) => {
+
+      expect(err).to.exist();
+    });
+  });
+
+  it('can start the server on a real port', () => {
+
+    const server = new Subpar('test');
+
+    const handler = function (request, reply) {
+
+      reply('hello');
+    };
+
+    server.handle(handler);
+
+    return server.start().then(() => {
+
+      expect(server.server.info.port).to.be.a.number();
+      expect(server.server.info.port).to.be.above(1000);
+    });
+  });
+});
